376561ddd3e85303f371af3b215a3aad0126ceab
Angelo Lee Salenga <91463946+gelolee@users.noreply.github.com>
2022-11-13 21:24:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import About from "./routes/About";
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
 
-import{Route, Switch, Routes} from "react-router-dom";
+import{Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <Switch>
+    <>
     <Routes>
       <Route path = "/" element = {<Home />} />
       <Route path = "/project" element = {<Project />} />
       <Route path = "/about" element = {<About />} />
       <Route path = "/contact" element = {<Contact />} />
     </Routes>
-    </Switch>
+    </>
   );
 }
 
